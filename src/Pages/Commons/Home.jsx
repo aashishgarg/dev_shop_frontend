b7d85a4bd12719dc6d4f3855abfb8c4f857209c6
Ashish Garg <ashish2.kumar@trantorinc.com>
2019-05-06 23:06:01 +0530

    code formatted

--- src/Pages/Commons/Home.jsx
+++ src/Pages/Commons/Home.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 import StartFeature from './Homes/StartFeature';
 import FeatureProd from './Homes/FeatureProd';
 import OfferArea from './Homes/OfferArea';
@@ -7,18 +7,18 @@ import NewProds from './Homes/NewProds';
 import InspiredProds from './Homes/InspiredProds';
 
 class Home extends Component {
-  render() {
-    return (
-      <div>
-        <Banner />
-        <StartFeature />
-        <FeatureProd />
-        <OfferArea />
-        <NewProds />
-        <InspiredProds />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Banner/>
+                <StartFeature/>
+                <FeatureProd/>
+                <OfferArea/>
+                <NewProds/>
+                <InspiredProds/>
+            </div>
+        )
+    }
 }
 
 export default Home
