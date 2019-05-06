83e890a1ad6083925af26cb42e7bba4b79073919
sanjayyadav19 <sanjay.yadav1@rsystems.com>
2019-05-06 19:53:09 +0530

    Theme implemented.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './App.css';
+import './Assets/css/bootstrap.css';
+import "./Assets/vendors/linericon/style.css"
+import "./Assets/css/font-awesome.min.css"
+import "./Assets/css/themify-icons.css"
+import "./Assets/css/flaticon.css"
+import "./Assets/vendors/owl-carousel/owl.carousel.min.css"
+import "./Assets/vendors/lightbox/simpleLightbox.css"
+import "./Assets/vendors/nice-select/css/nice-select.css"
+import "./Assets/vendors/animate-css/animate.css"
+import "./Assets/vendors/jquery-ui/jquery-ui.css"
+import "./Assets/css/style.css"
+import "./Assets/css/responsive.css"
+import Home from './Pages/Commons/Home';
+import Header from './Pages/Commons/Header';
+import Footer from './Pages/Commons/Footer';
+
+const Routing = props => (
+  <div>
+    <Switch>
+      {/* <Route path="/" exact component={Home} /> */}
+      {/* <Route path="/about" component={About} /> */}
+      {/* <Route path="/users" component={User} /> */}
+      {/* <Route path="/login" component={Login} /> */}
+    </Switch>
+  </div>
+)
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      {/* <SideNav/> */}
+        <Header />
+        <Home />
+        <Routing/>
+        <Footer/>
+    </Router>
   );
 }
 
