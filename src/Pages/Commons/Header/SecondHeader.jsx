7eabb46c8146851e7343da30949f910c9daa2d09
sanjayyadav19 <sanjay.yadav1@rsystems.com>
2019-05-07 11:14:18 +0530

    Get all images working.

--- src/Pages/Commons/Header/SecondHeader.jsx
+++ src/Pages/Commons/Header/SecondHeader.jsx
@@ -10,7 +10,7 @@ class SecondHeader extends Component {
                     <nav className="navbar navbar-expand-lg navbar-light w-100">
                         {/* Brand and toggle get grouped for better mobile display */}
                         <a className="navbar-brand logo_h" href="index.html">
-                            <img src="../../../Assets/img/logo.png"/>
+                            <img src={require('../../../Assets/img/logo.png')} />
                         </a>
                         <button className="navbar-toggler" type="button" data-toggle="collapse"
                                 data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
